5d700c1c72dc99939f182aa03f4e30aa35a93c61
zig wangles <153235983+zigwangles@users.noreply.github.com>
2024-11-18 22:53:03 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple script to handle form submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
